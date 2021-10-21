1d52d7b96c62af9a7054eb347d650acfd9a57b28
Rek9876boss <whteter@yahoo.com>
2021-10-21 08:23:35 -0400

    Update BBP Formulas.js

--- BBP Paper/BBP Formulas.js
+++ BBP Paper/BBP Formulas.js
@@ -1,14 +1,18 @@
 function firstForm(n){
   var temp = 0;
   for(let i = 0; i < n; i++){
-    temp = temp + ((1/(16^i)) * ((4/(8*i + 1))-(2/(8*i+4))-(1/(8*i+5))-(1/(8*i+6))));
+    temp = temp + ((1/(16^i))*((4/(8*i + 1))-(2/(8*i+4))-(1/(8*i+5))-(1/(8*i+6))));
   }
   return temp;
 }
 function secondForm(n){
   var temp = 0;
   for(let i = 0; i < n; i++){
-    temp = temp + ()
+    temp = temp + (((-1)^i)/(4^i))*((2/(4*i+1))+(2/(4*i+2))+(1/(4*i+3)));
   }
   return temp;
+}
+module.exports = {
+  f1_2: firstForm,
+  f1_9: secondForm,
 }
