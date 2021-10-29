62ca191aec6a2028629532419e392a5d1cbac860
Rek9876boss <whteter@yahoo.com>
2021-10-29 12:35:21 -0400

    commit on main

--- BBP Paper/BBP_Formulas.js
+++ BBP Paper/BBP_Formulas.js
@@ -20,7 +20,7 @@ class BBP{
    * @param {Number} n Number of iterations
    * @returns {Number}
    */
-  static f1_2(n){
+  f1_2(n){
     var temp = 0;
     for(let i = 0; i < n; i++){
      temp = temp + ((1/(16^i))*((4/(8*i + 1))-(2/(8*i+4))-(1/(8*i+5))-(1/(8*i+6))));
@@ -32,7 +32,7 @@ class BBP{
    * @param {Number} n Number of iterations
    * @returns {Number}
    */
-  static fupg905_4(n){
+  fupg905_4(n){
     var temp = 0;
     for(let i = 0; i < n; i++){
       temp = temp + (((-1)^i)/(4^i))*((2/(4*i+1))+(2/(4*i+2))+(1/(4*i+3)));
@@ -44,7 +44,7 @@ class BBP{
    * @param {Number} n Number of iterations
    * @returns {Number}
    */
-  static f2_3(n){
+  f2_3(n){
     return Math.sqrt(36*BBP.polyLogarithm(2,0.5,n) - 36*BBP.polyLogarithm(2,0.25,n) - 12*BBP.polyLogarithm(2,0.125,n) + 6*BBP.polyLogarithm(2,0.015625,n));
   }
   /**
@@ -52,7 +52,7 @@ class BBP{
    * @param {Number} n Number of iterations
    * @returns {Number}
    */
-  static fupg906_1(n){
+  fupg906_1(n){
     var temp = 0;
     for(let i = 0; i < n; i++){
       temp = temp + ((1/(64^i))*((16/((6*i+1)^2))-(24/((6*i+2)^2))-(8/((6*i+3)^3))-(6/((6*i+4)^2))+(1/((6*i+5)^2))));
