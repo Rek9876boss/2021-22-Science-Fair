a5aecf6e2f53eeaf7eb580affccff00e4f3d0332
Rek9876boss <whteter@yahoo.com>
2021-11-19 13:01:39 -0500

    Fix static functions

--- BBP Paper/BBP_Formulas.js
+++ BBP Paper/BBP_Formulas.js
@@ -22,7 +22,7 @@ class BBP{
    * @param {Number} n Number of iterations
    * @returns {Number}
    */
-  f1_2(n){
+  static f1_2(n){
     var temp = new Decimal('0');
     for(let i = 0; i < n; i++){
      temp = temp + ((1/(16^i))*((4/(8*i + 1))-(2/(8*i+4))-(1/(8*i+5))-(1/(8*i+6))));
@@ -34,7 +34,7 @@ class BBP{
    * @param {Number} n Number of iterations
    * @returns {Number}
    */
-  fupg905_4(n){
+  static fupg905_4(n){
     var temp = new Decimal('0');
     for(let i = 0; i < n; i++){
       temp = temp + (((-1)^i)/(4^i))*((2/(4*i+1))+(2/(4*i+2))+(1/(4*i+3)));
@@ -46,7 +46,7 @@ class BBP{
    * @param {Number} n Number of iterations
    * @returns {Number}
    */
-  f2_3(n){
+  static f2_3(n){
     return Math.sqrt(36*BBP.polyLogarithm(2,0.5,n) - 36*BBP.polyLogarithm(2,0.25,n) - 12*BBP.polyLogarithm(2,0.125,n) + 6*BBP.polyLogarithm(2,0.015625,n));
   }
   /**
@@ -54,7 +54,7 @@ class BBP{
    * @param {Number} n Number of iterations
    * @returns {Number}
    */
-  fupg906_1(n){
+  static fupg906_1(n){
     var temp = new Decimal('0');
     for(let i = 0; i < n; i++){
       temp = temp + ((1/(64^i))*((16/((6*i+1)^2))-(24/((6*i+2)^2))-(8/((6*i+3)^3))-(6/((6*i+4)^2))+(1/((6*i+5)^2))));
@@ -67,7 +67,7 @@ class BBP{
    * @param {Number} n Number of iterations
    * @returns {Number}
    */
-  f2_13(n){
+  static f2_13(n){
     function f(x){
       var temp = new Decimal('0');
       for(let i=1;i<n;i++){
