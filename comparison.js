39f01911796e9f8cfec04bf9aab679836d401b26
Rek9876boss <whteter@yahoo.com>
2021-11-19 13:32:56 -0500

    Create comparison.js

---
+++ comparison.js
@@ -0,0 +1,4 @@
+const mathjs = require("mathjs");
+const Decimal = require("decimal.js").Decimal;
+const BBP = require("./BBP Paper/BBP_Formulas.js").BBP;
+const Chu = require("./Chu Paper/Chu_Formulas.js").Chu;
