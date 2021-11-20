0776d0f8e4ddfed34180bea3b3c25202769fc500
Rek9876boss <whteter@yahoo.com>
2021-11-20 11:36:42 -0500

    added makeList
    Signed-off-by: Rek9876boss <whteter@yahoo.com>

--- comparison.js
+++ comparison.js
@@ -1,7 +1,12 @@
 const mathjs = require("mathjs");
 const Decimal = require("decimal.js").Decimal;
 const Pi = require("decimal.js").PI;
-set({precision:MAX_DIGITS, rounding: 1});
+Decimal.set({precision:MAX_DIGITS, rounding: 1});
+/**
+ * Compares different formulas
+ * @class
+ * @static
+ */
 class Comparator{
   /**
    * Measures accuracy at number of iterations
@@ -21,6 +26,26 @@ class Comparator{
       count++;
     }
     return [count,n];
+  }
+  /**
+   * 
+   * @param {Function} c1 
+   * @param {Function} c2 
+   * @param {number} n 
+   */
+  static makeList(c1,c2,n){
+    var fs = {
+      c1:[],
+      c2:[]
+    }
+    Object.getOwnPropertyNames(c2.prototype).forEach(function(value){if(value.charAt(0) == "f"){fs.c2.push(c2.value)}});
+    Object.getOwnPropertyNames(c1.prototype).forEach(function(value){if(value.charAt(0) == "f"){fs.c1.push(c1.value)}});
+    var fnList = {
+      c1: {},
+      c2: {}
+    }
+    fs.c1.forEach((value) => Object.defineProperty(fnList.c1,value,[]));
+    fs.c2.forEach((value) => Object.defineProperty(fnList.c2,value,[]));
   }
 }
 module.exports = {
