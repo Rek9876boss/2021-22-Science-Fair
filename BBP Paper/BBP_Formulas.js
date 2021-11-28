0bc6926dc00e580d908ab1baaf95363f0cb1aaec
Rek9876boss <whteter@yahoo.com>
2021-11-27 22:13:59 -0500

    formula overhaul
    Signed-off-by: Rek9876boss <whteter@yahoo.com>

--- BBP Paper/BBP_Formulas.js
+++ BBP Paper/BBP_Formulas.js
@@ -13,9 +13,9 @@ class BBP{
    * @returns {Decimal}
    */
   static polyLogarithm(m,z,a){
-    var temp = new Decimal('0');
+    var temp = new Decimal(0);
     for(let i = 0; i < a; i++){
-      temp = temp.plus(((z^i)/(i^m)));
+      temp = temp.add(((z^i)/(i^m)));
     }
     return temp;
   }
@@ -25,9 +25,9 @@ class BBP{
    * @returns {Decimal}
    */
   static f1_2(n){
-    var temp = new Decimal('0');
+    var temp = new Decimal(0);
     for(let i = 0; i < n; i++){
-     temp = temp.plus(((1/(16^i))*((4/(8*i + 1))-(2/(8*i+4))-(1/(8*i+5))-(1/(8*i+6)))));
+     temp = temp.add(((1/(16^i))*((4/(8*i + 1))-(2/(8*i+4))-(1/(8*i+5))-(1/(8*i+6)))));
     }
     return temp;
   }
@@ -37,9 +37,9 @@ class BBP{
    * @returns {Decimal}
    */
   static fupg905_4(n){
-    var temp = new Decimal('0');
+    var temp = new Decimal(0);
     for(let i = 0; i < n; i++){
-      temp = temp.plus(((((-1)^i)/(4^i))*((2/(4*i+1))+(2/(4*i+2))+(1/(4*i+3)))));
+      temp = temp.add(((((-1)^i)/(4^i))*((2/(4*i+1))+(2/(4*i+2))+(1/(4*i+3)))));
     }
     return temp;
   }
@@ -57,11 +57,11 @@ class BBP{
    * @returns {Decimal}
    */
   static fupg906_1(n){
-    var temp = new Decimal('0');
+    var temp = new Decimal(0);
     for(let i = 0; i < n; i++){
-      temp = temp.plus(((1/(64^i))*((16/((6*i+1)^2))-(24/((6*i+2)^2))-(8/((6*i+3)^3))-(6/((6*i+4)^2))+(1/((6*i+5)^2)))));
+      temp = temp.add(((1/(64^i))*((16/((6*i+1)^2))-(24/((6*i+2)^2))-(8/((6*i+3)^3))-(6/((6*i+4)^2))+(1/((6*i+5)^2)))));
     }
-    temp = temp.times(9/8);
+    temp = temp.mul(9/8);
     return Decimal.sqrt(temp);
   }
   /**
@@ -71,13 +71,13 @@ class BBP{
    */
   static f2_13(n){
     function f(x){
-      var temp = new Decimal('0');
+      var temp = new Decimal(0);
       for(let i=1;i<n;i++){
-        temp = temp.plus(((((-1)^i)*(x^i))/(2*i+1)));
+        temp = temp.add(((((-1)^i)*(x^i))/(2*i+1)));
       }
       return temp;
     }
-    return (Decimal.sqrt(2).times((f(0.5).times(4).plus(f(0.125)))));
+    return f(1/2).mul(4).add(f(1/8)).div(Decimal.sqrt(2));
   }
   functionList = {
     f1_2: BBP.f1_2,
